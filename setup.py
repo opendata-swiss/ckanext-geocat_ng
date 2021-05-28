3feb971b9f02709b6b05aee28e6afdc44ebbb22c
sabinem <sabine.maennel@gmail.com>
2021-05-28 08:34:10 +0200

    rename ckanext-geocat as ckanext-geocat_ng and adapt setup.py and test.ini

--- setup.py
+++ setup.py
@@ -9,18 +9,18 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='''ckanext-geocat''',
+    name='''ckanext-geocat_ng''',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
     version='0.0.1',
 
-    description='''CKAN harvester for geocat''',
+    description='''CKAN harvester for geocat next generation''',
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/ogdch/ckanext-geocat',
+    url='https://github.com/ogdch/ckanext-geocat_ng',
 
     # Author details
     author='''Liip AG''',
@@ -78,8 +78,8 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points='''
         [ckan.plugins]
-        geocat_harvester=ckanext.geocat.harvester:GeocatHarvester
+        geocat_ng_harvester=ckanext.geocat.harvester:GeocatNGHarvester
         [paste.paster_command]
-        geocat=ckanext.geocat.commands:GeocatCommand
+        geocat_ng=ckanext.geocat.commands:GeocatCommand
     ''',
 )
